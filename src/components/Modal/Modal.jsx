cc4bf61b8aa319be76584c90276a76794b73deb7
Anton Styazhkin <rudana089@gmail.com>
2023-06-19 21:22:50 +0300

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,43 +1,39 @@
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { Overlay, ModalContainer } from './Modal.styled';
 
-class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
+const Modal = ({ imageUrl, imageTags, onCloseModal }) => {
+  useEffect(() => {
+    const handleKeyDown = e => {
+      if (e.code === 'Escape') {
+        onCloseModal();
+      }
+    };
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [onCloseModal]);
 
-  handleKeyDown = e => {
-    if (e.code === 'Escape') {
-      this.props.onCloseModal();
-    }
-  };
-
-  handleClick = e => {
+  const handleClick = e => {
     if (e.target === e.currentTarget) {
-      this.props.onCloseModal();
+      onCloseModal();
     }
   };
 
-  render() {
-    const { imageUrl, imageTags } = this.props;
-
-    return (
-      <Overlay onClick={this.handleClick}>
-        <ModalContainer>
-          <img src={imageUrl} alt={imageTags} />
-        </ModalContainer>
-      </Overlay>
-    );
-  }
-}
+  return (
+    <Overlay onClick={handleClick}>
+      <ModalContainer>
+        <img src={imageUrl} alt={imageTags} />
+      </ModalContainer>
+    </Overlay>
+  );
+};
 
 Modal.propTypes = {
   imageUrl: PropTypes.string.isRequired,
+  imageTags: PropTypes.string.isRequired,
   onCloseModal: PropTypes.func.isRequired,
 };
 
